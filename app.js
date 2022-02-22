66d1d39003b51ca7d849c6ad9a0c68b0bba7f5a7
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-22 13:54:35 +0600

    add JSON.parse

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-alert("alert");
+const user = { name: 'mehedi', age: 26, color: 'black' }
+const stringfiyed = JSON.stringify(user);
+
+// console.log(user);
+// console.log(stringfiyed);
+
+
+const shop = {
+    name: 'bismilah store',
+    location: 'pod road',
+    product: [
+        'peaij',
+        'alu',
+        'oil',
+        'mosla',
+        'chal',
+        'dal'
+    ],
+    decoration: {
+        light: 'superStar',
+        self: 'rfl',
+        tala: 'docs'
+
+    },
+}
+const newStringify = JSON.stringify(shop);
+// console.log(shop);
+console.log(newStringify);
+const convert = JSON.parse(newStringify);
+console.log(convert);
